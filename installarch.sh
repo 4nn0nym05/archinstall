31b6c46fea2f3b140645242f1b9b58bc921bf913
4nn0nym05 <42974724+4nn0nym05@users.noreply.github.com>
2019-04-28 12:26:36 +0000

    Update installarch.sh

--- installarch.sh
+++ installarch.sh
@@ -16,3 +16,20 @@ genfstab -U -p /mnt >> /mnt/etc/fstab
 arch-chroot /mnt /bin/bash
 passwd
 pacman -S ttf-dejavu lxdm
+echo 'LANG="en_US.UTF-8"' >> /etc/locale.conf
+echo "en_US.UTF-8" >> /etc/locale.gen
+locale-gen
+ln -sf /usr/share/zoneinfo/Europe/Prague /etc/localtime
+hwclock --systohc --localtime
+echo "KEYMAP=cz" > /etc/vconsole.conf
+echo arch > /etc/hostname
+cat > /etc/hosts <<EOF
+127.0.0.1 localhost.localdomain localhost arch
+::1 localhost.localdomain localhost arch
+EOF
+pacman -S grub
+grub-install --recheck /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+exit
+umount -R /mnt
+reboot
