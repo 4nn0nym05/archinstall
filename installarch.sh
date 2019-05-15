1dbeef1c1fbccc5e1f558564d3d6c8154c34b8e1
MiGy <42974724+4nn0nym05@users.noreply.github.com>
2019-05-15 07:09:52 +0000

    Update installarch.sh

--- installarch.sh
+++ installarch.sh
@@ -14,22 +14,22 @@ mount /dev/sda1 /mnt
 pacstrap -i /mnt base base-devel
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
-arch-chroot "passwd"
-arch-chroot "pacman -S ttf-dejavu lxdm"
-arch-chrootecho "'LANG="en_US.UTF-8"' >> /etc/locale.conf"
-arch-chrootecho ""en_US.UTF-8" >> /etc/locale.gen"
-arch-chroot "locale-gen"
-arch-chrootln -sf /usr/share/zoneinfo/Europe/Prague /etc/localtime
-arch-chroothwclock --systohc --localtime
-arch-chrootecho "KEYMAP=cz" > /etc/vconsole.conf
-arch-chroot "echo arch > /etc/hostname"
-arch-chroot "cat > /etc/hosts <<EOF
+arch_chroot "passwd"
+arch_chroot "pacman -S ttf-dejavu lxdm"
+arch_chroot "echo 'LANG="en_US.UTF-8"' >> /etc/locale.conf"
+arch-chroot "echo "en_US.UTF-8" >> /etc/locale.gen"
+arch_chroot "locale-gen"
+arch_chroot "ln -sf /usr/share/zoneinfo/Europe/Prague /etc/localtime"
+arch_chroot "hwclock --systohc --localtime"
+arch_chroot "echo "KEYMAP=cz" > /etc/vconsole.conf"
+arch_chroot "echo arch > /etc/hostname"
+arch_chroot "cat > /etc/hosts <<EOF
 127.0.0.1 localhost.localdomain localhost arch
 ::1 localhost.localdomain localhost arch
 EOF"
-arch-chroot "pacman -S grub"
-arch-chroot "grub-install --recheck /dev/sda"
-arch-chroot "grub-mkconfig -o /boot/grub/grub.cfg"
+arch_chroot "pacman -S grub"
+arch_chroot "grub-install --recheck /dev/sda"
+arch_chroot "grub-mkconfig -o /boot/grub/grub.cfg"
 exit
 umount -R /mnt
 reboot
